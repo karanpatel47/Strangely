8a46e0fafaa4fd884a4be611671792aa4fc9cc8b
KARAN PATIDAR <karanpatidar9399@gmail.com>
2026-09-05 17:06:56 +0530

    Switch to MaintenancePage in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import ChatRoom from './pages/ChatRoom'
-
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/chat" element={<ChatRoom />} />
-      <Route path="*" element={<Home />} />
-    </Routes>
-  )
-}
-// import MaintenancePage from './components/MaintenancePage'
+// import { Routes, Route } from 'react-router-dom'
+// import Home from './pages/Home'
+// import ChatRoom from './pages/ChatRoom'
 
 // export default function App() {
-//   return <MaintenancePage />
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/chat" element={<ChatRoom />} />
+//       <Route path="*" element={<Home />} />
+//     </Routes>
+//   )
 // }
+import MaintenancePage from './components/MaintenancePage'
+
+export default function App() {
+  return <MaintenancePage />
+}
